d0025b0d82f3f8c3b398d1d6c1e2e24d22f3ceba
Kellerrel <kellerken.kk@gmail.com>
2026-05-11 22:12:49 +0300

    Pass Data to Views

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
+
+Route::get('/', function () {
+    return view('welcome', [
+        'greeting' => 'Hello',
+    'person' => request('person', 'World'),
+    ]);
+
+});
