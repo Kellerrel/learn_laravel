7fc03190a9dfc93cf5c7f192d27720720f80fd52
Kellerrel <kellerken.kk@gmail.com>
2026-05-14 21:08:52 +0300

    Controllers

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -24,7 +24,7 @@
     </style>
 </head>
 
-<body class="bg-gray-700 p-6 max-w-xl mx-auto">
+<body class="bg-gray-700 p-6 max-w-xl mx-auto text-white">
     <nav>
         <a href="/">Home</a>
         <a href="/about">About Us</a>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 //use Illuminate\Support\Facades\DB;
 //use Illuminate\Support\Facades\Request;
+use App\Http\Controllers\IdeaController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Idea;
 
@@ -10,80 +11,16 @@ Route::view('/about', 'about');
 Route::view('/contact', 'contact');
 
 Route::get('/', function () {
-    return view('welcome', [
-        'tasks' => [
-            'Go to the market',
-            'Walk the dog',
-            'Watch a video tutorial',
-        ],
-    ]);
+    return view('welcome');
 
 });
 
-
-// index
-Route::get('/ideas', function () {
-    // $ideas = session()->get('ideas', []);
-    // $ideas = DB::table('ideas')->get();
-    // $ideas = Idea::where('state','pending')->get(); //if spesific, can't use all
-    $ideas = Idea::all();
-
-    return view('ideas.index', [
-    'ideas'=>$ideas,
-    ]);
-});
-
-//show action
-Route::get('/ideas/{idea}', function (Idea $idea) {
+Route::get('/ideas', [IdeaController::class, 'index']);
+Route::get('/ideas/create', [IdeaController::class,'create']);
+Route::post('/ideas', [IdeaController::class,'store']);
+Route::get('/ideas/{idea}', [IdeaController::class,'show']);
+Route::get('/ideas/{idea}/edit',[IdeaController::class,'edit']);
+Route::patch('/ideas/{idea}',[IdeaController::class,'update']);
+Route::delete('/ideas/{idea}',[IdeaController::class,'delete']);
 
 
-    //$idea = Idea::findOrFail($id); //find or if not found 404 error
-
-    //if(is_null($idea)) {
-    //    abort(404);
-    //}
-
-    return view('ideas.show', [
-    'idea'=>$idea,
-    ]);
-});
-
-//edit action
-Route::get('/ideas/{idea}/edit', function (Idea $idea) {
-
-    return view('ideas.edit', [
-    'idea'=>$idea,
-    ]);
-});
-
-//update action
-Route::patch('/ideas/{idea}', function (Idea $idea) {
-    $idea->update([
-        'description'=> request('description'),
-    ]);
-    return redirect("/ideas/{$idea->id}");
-});
-
-//store action
-Route::post('/ideas', function () {
-    //$idea = request('idea'); //grab the idea
-
-    Idea::create([
-    'description'=>request('idea'),
-    'state'=>'pending',
-    ]);
-    return redirect('/ideas'); //return to home
-});
-
-//destroy action
-Route::delete('/ideas/{idea}', function (Idea $idea) {
-    $idea->delete();
-
-    return redirect('/ideas'); //return to home
-});
-
-//// Temporary
-//Route::get('/delete-ideas', function () {
-//    Idea::truncate();
-//    return redirect('/');
-//});
